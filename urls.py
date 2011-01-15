9500cf6c71f413c1eb4724eda8bffd887dd32511
Hayes Williams <dubious.pie@gmail.com>
2011-01-15 02:18:27 -0800

    Updated urls.py

--- urls.py
+++ urls.py
@@ -5,12 +5,17 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^', 'main.views.index'),
+    (r'^tagcloudxml', 'main.views.tagcloudxml')
+	(r'^login', 'main.views.login.login'),
+	(r'^login/register', 'main.views.login.register ')
+	(r'^login/activate', 'main.views.login.activate')
     (r'^view_event/(?P<event_id>\d+)', 'main.views.events.view_event'),
     (r'^add_event/', 'main.views.events.add_event'),
-	(r'^browse_events/', 'main.views.events.browse_events')
+    (r'^browse_events/(?P<tag>.*)/', 'main.views.events.browse_events')
     (r'^view_venue/(?P<location_id>\d+)', 'main.views.venues.view_venue'),
     (r'^add_venue/', 'main.views.venues.add_venue'),
-	(r'^browse_venues/', 'main.views.venues.browse_venue')
+    (r'^browse_venues/', 'main.views.venues.browse_venue')
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls))
     )
+
