36cc2b4d5874995726c177504617ea2b852b3e7e
Hayes Williams <dubious.pie@gmail.com>
2011-03-01 09:16:24 -0800

    Added a new form, part one of the registration wizard.

--- main/forms.py
+++ main/forms.py
@@ -8,3 +8,10 @@ class LoginForm(forms.Form):
     username = forms.CharField(label=_(u'Username'), widget=AutofocusInput)
     password = forms.CharField(label=_(u'Password'), widget=forms.PasswordInput(render_value=False)) 
 
+class RegisterForm1(forms.Form):
+    username = forms.CharField(label=_(u'Username'))
+    email = EmailField(label=_(u"Email"))
+    password = forms.CharField(label=_(u'Password'), widget=forms.PasswordInput(render_value=False)) 
+    passwordconfirm = forms.CharField(label=_(u'Password'), widget=forms.PasswordInput(render_value=False))
+    recaptcha = ReCaptchaField()
+    
