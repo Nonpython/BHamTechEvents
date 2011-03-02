b033cd1674beda4802b31a242a5f42fb272a9791
Hayes Williams <dubious.pie@gmail.com>
2011-03-01 17:33:34 -0800

    I only need one form for registration.

--- main/forms.py
+++ main/forms.py
@@ -8,10 +8,13 @@ class LoginForm(forms.Form):
     username = forms.CharField(label=_(u'Username'), widget=AutofocusInput)
     password = forms.CharField(label=_(u'Password'), widget=forms.PasswordInput(render_value=False)) 
 
-class RegisterForm1(forms.Form):
+class RegisterForm(forms.Form):
     username = forms.CharField(label=_(u'Username'))
-    email = EmailField(label=_(u"Email"))
+    email = forms.EmailField(label=_(u"Email"))
     password = forms.CharField(label=_(u'Password'), widget=forms.PasswordInput(render_value=False)) 
-    passwordconfirm = forms.CharField(label=_(u'Password'), widget=forms.PasswordInput(render_value=False))
-    recaptcha = ReCaptchaField()
+    passwordconfirm = forms.CharField(label=_(u'Confirm your password'), widget=forms.PasswordInput(render_value=False))
+    realname =  forms.CharField(label=_(u'Real name'))
+    bio = forms.CharField(label=_(u'Tell us a little about yourself'), widget=forms.Textarea())
+    recaptcha = ReCaptchaField(label=_(u'Are you a human?'))
+    
     
