0aa56bca9aece91d7ce07f956d86e0f613a5d29e
Hayes Williams <dubious.pie@gmail.com>
2011-03-02 22:09:19 -0800

    Restructured the urls.

--- urls.py
+++ urls.py
@@ -4,20 +4,20 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^', 'main.views.index'),
+    (r'^index', 'main.views.shared.index'),
     (r'^tagcloudxml', 'main.views.tagcloudxml')
-    (r'user', 'main.views.user')
-	(r'^login', 'main.views.login.login'),
-	(r'^register', 'main.views.login.register ')
-	(r'^register/activate', 'main.views.login.activate')
-    (r'^view_event/(?P<event_id>\d+)', 'main.views.events.view_event'),
-    (r'^add_event/', 'main.views.events.add_event'),
-    (r'^browse_events/', 'main.views.events.browse_events')
-    (r'^search_events/(?P<tag>.*)/', 'main.views.events.search_events')
-    (r'^view_venue/(?P<location_id>\d+)', 'main.views.venues.view_venue'),
-    (r'^add_venue/', 'main.views.venues.add_venue'),
-    (r'^browse_venues/', 'main.views.venues.browse_venue')
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^admin/', include(admin.site.urls))
+    #(r'user', 'main.views.user')
+	#(r'^login', 'main.views.login.login'),
+	#(r'^register', 'main.views.login.register ')
+	#(r'^register/activate', 'main.views.login.activate')
+    #(r'^events/view/(?P<event_id>\d+)', 'main.views.events.view_event'),
+    #(r'^events/add', 'main.views.events.add_event'),
+    #(r'^events/browse', 'main.views.events.browse_events')
+    #(r'^events/bytag/(?P<tag>.*)/', 'main.views.events.browse_events_by_tag')
+    #(r'^venues/view/(?P<location_id>\d+)', 'main.views.venues.view_venue'),
+    #(r'^venues/add', 'main.views.venues.add_venue'),
+    #(r'^venues/browse', 'main.views.venues.browse_venue')
+    #(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #(r'^admin/', include(admin.site.urls))
     )
 
