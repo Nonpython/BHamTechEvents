984365cc747a5db74bcc4afd7b0d1b323a151ff9
Hayes Williams <dubious.pie@gmail.com>
2011-03-08 19:58:37 -0800

    Fixed tabs vs. spaces issue.

--- main/management/commands/update_events.py
+++ main/management/commands/update_events.py
@@ -11,44 +11,44 @@ class Command(BaseCommand):
         requires_model_validation = True
         help = 'Checks for events with less than NUM_OCCURANCES Occurances, and creates more' + \
         ' so that it has the correct number.'
-		
+
         def handle(self, *args, **options):
             from django.conf import settings
             occurances = {}
-        	for event in Event.objects.all():
-	                        for occurance in event.occurances.all():
-	                                try:
-	                                        occurances[occurance.of.name].append(occurance)
-	                                except KeyError:
-	                                        occurances[occurance.of.name] = [occurance]
-	        for occurances_of_event in occurances.values():
-	                occurances_of_event.sort(key=occurances.date)
-	                for occurance in occurances_of_event:
-	                        if datetime.combine(occurance.date, occurance.of.start) < datetime.now(settings.TIME_ZONE):
-	                                occurance.delete()
-	                                print u'Removed Occurance of event "%s" at %s' % (occurance.of.name,
-	                                           datetime.combine(occurance.date, occurance.of.start))
-	                if len(occurances_of_event) < getattr(settings, "MIN_EVENT_OCCURANCES", 3):
-	                 	latest_occurance = occurances_of_event[-1]
-	                    if latest_occurance.of.freq == "DAILY":
-	                    	rrule = dateutil.rrule.rrule(dateutil.rrule.DAILY,
+            for event in Event.objects.all():
+                            for occurance in event.occurances.all():
+                                    try:
+                                            occurances[occurance.of.name].append(occurance)
+                                    except KeyError:
+                                            occurances[occurance.of.name] = [occurance]
+            for occurances_of_event in occurances.values():
+                    occurances_of_event.sort(key=occurances.date)
+                    for occurance in occurances_of_event:
+                            if datetime.combine(occurance.date, occurance.of.start) < datetime.now(settings.TIME_ZONE):
+                                    occurance.delete()
+                                    print u'Removed Occurance of event "%s" at %s' % (occurance.of.name,
+                                               datetime.combine(occurance.date, occurance.of.start))
+                    if len(occurances_of_event) < getattr(settings, "MIN_EVENT_OCCURANCES", 3):
+                        latest_occurance = occurances_of_event[-1]
+                        if latest_occurance.of.freq == "DAILY":
+                            rrule = dateutil.rrule.rrule(dateutil.rrule.DAILY,
                                                          count=1,
                                                          interval=latest_occurance.of.interval
                                                          )
-	                    elif latest_occurance.of.freq == "WEEKLY":
-	                    	rrule = dateutil.rrule.rrule(dateutil.rrule.WEEKLY,
+                        elif latest_occurance.of.freq == "WEEKLY":
+                             rrule = dateutil.rrule.rrule(dateutil.rrule.WEEKLY,
                                                          count=1,
                                                          byweekday=getattr(dateutil.rrule, latest_occurance.of.rule),
                                                          interval=latest_occurance.of.interval
                                                          )
-	                    elif latest_occurance.of.freq == "MONTHLY":
+                        elif latest_occurance.of.freq == "MONTHLY":
                             rrule = dateutil.rrule.rrule(dateutil.rrule.MONTHLY,
                                                          count=1,
                                                          byweekday=getattr(dateutil.rrule, latest_occurance.of.rule[1:]),
                                                          bysetpos=int(latest_occurance.of.rule[:1]),
                                                          interval=latest_occurance.of.interval
                                                          )
-	                    elif latest_occurance.of.freq == "YEARLY":
+                        elif latest_occurance.of.freq == "YEARLY":
                             match = re.match(r'(\d\d?)([MO|TU|WE|TH|FR|SA|SU])', latest_occurance.of.interval)
                             rrule = dateutil.rrule.rrule(dateutil.rrule.YEARLY,
                                                          count=1,
@@ -63,4 +63,3 @@ class Command(BaseCommand):
                         occurance.save()
                         print 'Added Occurance of Event "%s" on %s'  % (latest_occurance.of.name, occurance.date)
 
-                
